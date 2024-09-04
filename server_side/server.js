59a846b9f7f3b1ebe60c9a7bb3848e839f7a84e2
shifas <sshifas333@gmail.com>
2024-09-04 16:43:56 +0530

    new file added old file updated

---
+++ server_side/server.js
@@ -0,0 +1,93 @@
+const PORT=3000;
+const http=require("http");
+const fs=require("fs");
+const url=require("url");
+const queryString=require("querystring");
+const {MongoClient}=require("mongodb");
+const { error } = require("console");
+
+//connect mmongodb
+const client = new MongoClient("mongodb://127.0.0.1:27017/");
+
+const app=http.createServer(async(req,res)=>{
+    //create database
+    const db=client.db("B_bank");
+    //create collection
+    const collection=db.collection("donors");
+const path=url.parse(req.url);
+console.log(path);
+if(path.pathname=="/")
+    {
+    res.writeHead(200,{"Content-Type":"text/html"});
+    res.end(fs.readFileSync("../client_side/index.html"));
+
+}
+else if(path.pathname=="/js/customer.js")
+    {
+    res.writeHead(200,{"Content-type":"text/js"});
+    res.end(fs.readFileSync("../client_side/js/customer.js"));
+}
+else if(path.pathname=="/css/index.css")
+    {
+res.writeHead(200,{"Content-Type":"text/css"});
+res.end(fs.readFileSync("../client_side/css/index.css"));
+}
+else if(path.pathname=="/add")
+    {
+   res. writeHead(200,{"Content-Type":"text/html"})
+   res.end(fs.readFileSync("../client_side/pages/user.html"));
+}
+else if(path.pathname=="/css/user.css")
+{
+    res.writeHead(200,{"Content-Type":"text/css"});
+    res.end(fs.readFileSync("../client_side/css/user.css"));
+}
+console.log(req.method);
+
+if(req.method=="POST" && path.pathname=="/submit"){
+    
+    let body="";
+    req.on("data",(chunks)=>{
+        console.log(chunks);
+        body +=chunks.toString();
+        console.log(body);   
+    });
+    req.on("end",async()=>{
+        if(body!=null){
+            //convert to object
+            const formData=queryString.parse(body);
+            console.log(formData);
+            //insert data
+            collection.insertOne(formData).then(()=>{
+                console.log("success");
+                
+            })
+            .catch((error)=>{
+                console.log(error);
+                
+            })
+            
+        }
+    })
+    res.writeHead(200,{"Content-Type":"text/html"});
+    res.end(fs.readFileSync("../client_side/index.html"))
+}
+// get data from the database and send it into the front-end
+if (path.pathname == "/getdonors" && req.method == "GET"){
+    const data = await collection.find().toArray();
+    const jasonData= JSON.stringify(data);
+    console.log(jasonData);
+    res.writeHead(200,{"Content-type":"text/json"});
+    res.end(jasonData);   
+}
+});
+
+//connecting database
+client.connect().then(()=>{
+    console.log("database connected");
+    app.listen(PORT,()=>{
+        console.log(`http://localhost:${PORT}`);   
+    });
+}).catch((error)=>{
+    console.log(error);   
+})
